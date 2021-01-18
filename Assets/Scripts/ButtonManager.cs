f0e465e76ad69d670ce3f07e403bb718b10a61aa
Rossco1337 <rossco1337@gmail.com>
2021-01-18 03:51:05 +0000

    super basic menu + preferences skeleton

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void StartBattleBtn(string name)
+    {
+        Debug.Log($"Loading scene { name }");
+        SceneManager.LoadScene(name);
+    }
+
+    public void SavePrefsBtn(int player1CurHp)
+    {
+        Debug.Log($"Saving prefs");
+        PlayerPrefs.SetInt("player1CurHp", player1CurHp);
+        PlayerPrefs.Save();
+    }
+
+    public void PrintPrefsBtn()
+    {
+        Debug.Log($"PlayerPrefs:");
+        Debug.Log($"Player 1 Current HP: { PlayerPrefs.GetInt("player1CurHp") } ");
+    }
+
+    public void ClearPrefsBtn()
+    {
+        Debug.Log($"Purging prefs");
+        PlayerPrefs.DeleteAll();
+    }
+}
