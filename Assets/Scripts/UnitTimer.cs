dc6fa65467ec78934538104f67e1c8c632bcb88c
Rossco1337 <rossco1337@gmail.com>
2021-01-24 09:56:41 +0000

    this should probably be a scriptableobject so i'm gonna brainstorm it more before i work on it

--- Assets/Scripts/UnitTimer.cs
+++ Assets/Scripts/UnitTimer.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitTimer {
+public class UnitTimer : MonoBehaviour
+{
+    [Range(0,65535)]
+    public ushort vTime, vUnits, cTime, cUnits, turnTimer;
+
+    /* don't know enough to use these in the inspector yet
     public struct VariableTimer {
         public ushort vTime;
         public uint vUnits;
-
     }
+    */
 
     // public int TurnTimer
     // {
